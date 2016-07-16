260810043ec7aec940cf2af2c9e8204d08561b68
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-16 13:18:39 -0700

    Hardcoded profile data

---
+++ models/rolemodel.js
@@ -0,0 +1,17 @@
+var Rolemodel = mongoose.model('Rolemodel', RolemodelSchema);
+
+
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var RolemodelSchema = new Schema({
+    name : String,
+    title: String,
+    nationality: String,
+    description: String,
+    img  : String,
+});
+
+
+module.exports = Rolemodel;
