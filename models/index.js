9cce28dd429e8825129821a1aca369a5f5fd7b2d
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-16 15:32:06 -0700

    create profile.js for later use

--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
 var mongoose = require("mongoose");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api"); 
  mongoose.connect( process.env.MONGODB_URI || "YOUR CURRENT LOCALHOST DB CONNECTION STRING HERE" );
 
-
 //module.exports.Campsite = require("./campsite.js.example");
+
+module.exports.Rolemodel=require('/.rolemodel.js');
+//module.exports.Profile=require('/.Profile.js');
