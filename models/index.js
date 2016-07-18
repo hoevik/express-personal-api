c91625a4cc576f7098a7d2c699a3602874ff04be
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-17 20:33:40 -0700

    making mvp

--- models/index.js
+++ models/index.js
@@ -1,7 +1,12 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
+// mongoose.connect( process.env.MONGOLAB_URI ||
+//                   process.env.MONGOHQ_URL ||
+//                   "mongodb://localhost/personal-api");
+
+
+mongoose.connect( process.env.MONGODB_URI|| "mongodb://localhost/personal-api");
 
 //module.exports.Campsite = require("./campsite.js.example");
 
-module.exports.Rolemodel=require('./rolemodel.js');
-module.exports.Profile=require('./profile.js');
+module.exports.Rolemodel = require('./rolemodel.js');
+module.exports.Profile = require('./profile.js');
--- models/rolemodel.js
+++ models/rolemodel.js
@@ -1,13 +1,13 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
-var RolemodelSchema = new Schema({
-    name : String,
-    title: String,
-    nationality: String,
-    description: String,
-    img  : String,
-    added_by: String,
+var RolemodelSchema = new Schema ({
+  name : String,
+  title: String,
+  nationality: String,
+  description: String,
+  img  : String,
+  added_by: String,
 });
 
 var Rolemodel = mongoose.model('Rolemodel', RolemodelSchema);
